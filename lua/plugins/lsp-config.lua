2a825c0310b43f91684cfc51de73bd5dff602504
Omegalolchan <pedrolucasrochap+git@gmail.com>
2024-10-29 00:42:33 -0300

    nvim

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,34 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup()
+		end
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+			local lspconfig = require('lspconfig')
+			lspconfig.gdscript.setup({
+				capabilities = capabilities
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities
+			})
+			
+			vim.keymap.set('n', '<C-up>', vim.lsp.buf.hover, {})
+			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {}) 
+			vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
+			
+			vim.keymap.set('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', {})
+		end
+	}
+}
